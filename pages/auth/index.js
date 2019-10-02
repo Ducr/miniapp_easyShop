2f88200bc7ad45d3e12a506829589e3ae0d41c74
Ducr <Dulant_du@126.com>
2019-10-03 04:46:04 +0800

    08.完成授权页面的用户授权功能

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,66 +1,32 @@
 // pages/auth/index.js
+import regeneratorRuntime from "./../../lib/runtime/runtime"
+import { login, request } from './../../request/myRequest.js'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  // 获取用户信息 ------ 使用 button  的开放能力
+  handleGetuserinfo(e) {
+    // 将 返回的参数e 用于 微信小程序登录 的 请求参数
+    this.wxMiniAppLogin(e)
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 微信小程序登录
+  async  wxMiniAppLogin(e) {
+    // 构建登录请求参数
+    // 参数 code 执行小程序登录后获取
+    const { code } = await login()
+    // 其余参数直接从 用户信息 授权获取
+    const { encryptedData, iv, rawData, signature } = e.detail
+    const tokenParams = { encryptedData, iv, rawData, signature, code }
+    const res = await request({
+      url: '/users/wxlogin',
+      method: 'post',
+      data: tokenParams
+    })
+    // console.log(res)
+    // 存储到本地储存中
+    wx.setStorageSync('token', res.token)
+    // 授权后进行跳转
+    wx.navigateBack({
+      // 返回上一页
+      delta: 1
+    })
   }
 })
